a244b5c0f8cbcabfb9417c159f0e4e256a79913f
dbrsseau <dbrsseau@gmail.com>
2021-01-24 02:52:24 -0500

    Fix memory leaks

--- src/pipeline.c
+++ src/pipeline.c
@@ -16,24 +16,37 @@
 #define MAX_LENGTH 255 /**< Buffer maximum length */
 
 // Function declarations
-void groupFrequencies(int * pipe);
-void printLetters(int * frequencies);
-void summateDigits(int * pipe);
+void groupFrequencies(int *pipe);
+void printLetters(int *frequencies);
+void summateDigits(int *pipe);
 
 /**
  * @brief Create all process childs
  * @param pipe1 Pipe between child 1 and his parent
  * @param pipe2 Pipe between child 2 and his parent
+ * REMINDER: The pipes must be freed in each child to avoid memory leaks.
  */
-void createChilds(int * pipe1, int * pipe2) {
+void createChilds(int *pipe1, int *pipe2) {
     int child1 = fork();
-    int status;
     if (child1 == 0) {
         summateDigits(pipe1);
+        close(pipe1[0]);
+        free(pipe1);
+        free(pipe2);
+        exit(EXIT_SUCCESS);
     } else {
-        waitpid(child1, &status, WNOHANG | WUNTRACED);
+        wait(NULL);
+        free(pipe1);
         int child2 = fork();
-        if (child2 == 0) { groupFrequencies(pipe2); }
+        if (child2 == 0) {
+            groupFrequencies(pipe2);
+            close(pipe2[0]);
+            free(pipe2);
+            exit(EXIT_SUCCESS);
+        } else {
+            wait(NULL);
+            free(pipe2);
+        }
     }
 }
 
@@ -41,8 +54,8 @@ void createChilds(int * pipe1, int * pipe2) {
  * @brief Create a new pipe.
  * @return File descriptor
  */
-int * createPipe() {
-    int * fd = calloc(2, sizeof(int));
+int *createPipe() {
+    int *fd = calloc(2, sizeof(int));
     if (pipe(fd) != 0) {
         fprintf(stderr, "pipeline: fd: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
@@ -54,7 +67,7 @@ int * createPipe() {
  * @brief Group letter frequencies within the second child.
  * @param pipe Pipe between child and his parent
  */
-void groupFrequencies(int * pipe) {
+void groupFrequencies(int *pipe) {
     close(pipe[1]);
     char letters[MAX_LENGTH] = "";
     read(pipe[0], letters, MAX_LENGTH);
@@ -74,14 +87,13 @@ void groupFrequencies(int * pipe) {
         l++;
     }
     printLetters(frequencies);
-    exit(EXIT_SUCCESS);
 }
 
 /**
  * @brief Print the letters to stdout stream.
  * @param frequencies Letter frequencies
  */
-void printLetters(int * frequencies) {
+void printLetters(int *frequencies) {
     for (int i = 0; i < 26; i++) // Lower case
     {
         if (frequencies[i] > 0) { printf("La lettre %c apparaît %d fois.\n", i + 'a', frequencies[i]); }
@@ -97,14 +109,14 @@ void printLetters(int * frequencies) {
  * @param filename File to read
  * @return Content buffer
  */
-char * readFile(const char * filename) {
-    FILE * file;
+char *readFile(const char *filename) {
+    FILE *file;
     file = fopen(filename, "r");
     if (file == NULL) {
         fprintf(stderr, "pipeline: %s: %s\n", filename, strerror(errno));
         exit(EXIT_FAILURE);
     }
-    char * buffer = calloc(MAX_LENGTH, sizeof(char));
+    char *buffer = calloc(MAX_LENGTH, sizeof(char));
     fread(buffer, MAX_LENGTH, 1, file);
     fclose(file);
     return buffer;
@@ -114,14 +126,13 @@ char * readFile(const char * filename) {
  * @brief Summate all digits within the first child.
  * @param pipe Pipe between child and his parent
  */
-void summateDigits(int * pipe) {
+void summateDigits(int *pipe) {
     close(pipe[1]);
     char digit[MAX_LENGTH] = "";
     read(pipe[0], digit, MAX_LENGTH);
     int sum = 0;
     for (unsigned long i = 0; i < (strlen(digit)); i++) { sum = sum + (digit[i] - 48); }
     if (sum > 0) { fprintf(stdout, "La somme des chiffres est %d.\n", sum); }
-    exit(EXIT_SUCCESS);
 }
 
 /**
@@ -130,14 +141,14 @@ void summateDigits(int * pipe) {
  * @param argv File to interpret
  * @return Exit status
  */
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: bin/pipeline <filename>\n");
         exit(EXIT_FAILURE);
     }
-    char * buffer = readFile(argv[1]);
-    int * pipe1 = createPipe();
-    int * pipe2 = createPipe();
+    char *buffer = readFile(argv[1]);
+    int *pipe1 = createPipe();
+    int *pipe2 = createPipe();
     for (unsigned long i = 0; i < strlen(buffer); i++) {
         if (isdigit(buffer[i])) {
             write(pipe1[1], &buffer[i], 1);
@@ -145,12 +156,8 @@ int main(int argc, char * argv[]) {
             write(pipe2[1], &buffer[i], 1);
         }
     }
-    createChilds(pipe1, pipe2);
-    close(pipe1[0]);
-    close(pipe2[0]);
-    wait(0);
+    // REMINDER: The buffer must be freed before the creation of the childs to avoid memory leaks.
     free(buffer);
-    free(pipe1);
-    free(pipe2);
+    createChilds(pipe1, pipe2);
     return EXIT_SUCCESS;
 }
